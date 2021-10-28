c46ebcd94d1d25d12d1a1fd2a4fb8cfd359fc117
Faber <a.a.r.samuel99@gmail.com>
2021-10-27 22:57:29 -0500

    mejora NN

--- src/autonomos_gazebo_simulation/scripts/fullKeras.py
+++ src/autonomos_gazebo_simulation/scripts/fullKeras.py
@@ -5,7 +5,6 @@ import numpy as np
 from sensor_msgs.msg import Image
 from std_msgs.msg import Int16
 import tensorflow as tf
-import cv2
 
 FT = 0
 l = 60 
@@ -17,15 +16,11 @@ x2_h = 120
 
 h_vis =1.0/30.0
 u = 90
-v = -400 
+v = -600 
 
 k = 1
 
-interpreter = tf.lite.Interpreter('models/model2RGB.tflite')
-interpreter.allocate_tensors()
-input_details = interpreter.get_input_details()
-print('Input details: ', input_details)
-output_details = interpreter.get_output_details()
+model = tf.keras.models.load_model('models/model2RGB.h5', compile = False)
 
 def callback_V(data0):
 	global u, v
@@ -35,15 +30,16 @@ def callback_V(data0):
 	global interpreter, input_details, output_details
 	
 	im = np.frombuffer(data0.data, dtype=np.uint8,).reshape(data0.height, data0.width, -1)
-	# im = cv2.cvtColor(im, cv2.COLOR_BGR2YUV)
+	#imagen0 = bridge.imgmsg_to_cv2(data0, "bgr8")
+	#imagen0 = cv2.cvtColor(imagen0, cv2.COLOR_BGR2RGB)
+	#im = imagen0.astype(np.float32)
+	im = im.astype('float32')
 	im = im[270:405, :, :]
+	#im = tf.image.rgb_to_yuv(im)
+	#im = cv2.cvtColor(im, cv2.COLOR_BGR2YUV)
 	im = cv2.resize(im, (200,66), interpolation = cv2.INTER_AREA)
 	cv2.imshow("img", im)
 	cv2.waitKey(1)
-	im = im.astype('float32')
-	
-	#im = tf.image.rgb_to_yuv(im)
-	
 	im = np.expand_dims(im, axis = 0)	
 	#print(im.shape)
 	im = (im/127.5) - 1
@@ -51,11 +47,11 @@ def callback_V(data0):
 	#im = tf.cast(im, tf.float32)
 	#im = tf.expand_dims(im, axis = 0)
 	#print()
-	interpreter.set_tensor(input_details[0]['index'], im)
-	interpreter.invoke()
-	u = interpreter.get_tensor(output_details[0]['index'])[0][0]
-	u = ((u + 1)/2)*180 + 45
-	#u = int16(u)
+	#interpreter.set_tensor(input_details[0]['index'], im)
+	#interpreter.invoke()
+	u = model(im, training = False)[0][0]
+	u = ((u + 1)/2)*180
+	u = int(u)
 	print('steering ',u)
 
 	Vpub.publish(v) 
